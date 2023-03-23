8bd4f76ca389f1a7dd451f06005e7e352c99860f
hippo2020 <badjh2005@163.com>
2023-03-23 20:34:30 +0800

    modified:   __pycache__/main.cpython-39.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 '''
 Date: 2023-03-20 20:20:46
 LastEditors: jhao
-LastEditTime: 2023-03-20 20:21:36
-FilePath: \python\hello.py
+LastEditTime: 2023-03-23 20:33:32
+FilePath: \python\main.py
 '''
+from typing import Optional
 from fastapi import FastAPI
 
 app=FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message":"Hello World"}
+fake_items_db=[{"item_name":"Foo"},{"item_name":"Bar"},{"item_name":"Baz"}]
+
+@app.get("/items/{skip}&{limit}")
+async def read_item(skip:int=0,limit:int=10):
+    return fake_items_db[skip:skip+limit]
 
